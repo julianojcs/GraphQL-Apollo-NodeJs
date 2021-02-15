553d4e12f946ff70201ff8417483d29a2ea78ba8
Juliano Costa Silva <apfjuliano@gmail.com>
2021-02-15 02:30:26 -0300

    Mutation excluirUsuario

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -16,5 +16,14 @@ module.exports = {
     }
     usuarios.push(novo)
     return novo
+  },
+
+  excluirUsuario: (root, {id}) => {
+    const i = usuarios
+      .findIndex(u => u.id === id)
+    
+    if(i<0) return null
+    const excluidos = usuarios.splice(i, 1)
+    return excluidos ? excluidos[0] : null
   }
 }
