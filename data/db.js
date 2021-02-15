66e737608698524f20e6475ac97b1d0b9dbc8f9d
Juliano Costa Silva <apfjuliano@gmail.com>
2021-02-15 02:14:02 -0300

    Mutation novoUsuario

--- data/db.js
+++ data/db.js
@@ -11,7 +11,7 @@ const usuarios = [
     idade: 44,
     salario_real: 19230.9,
     vip: true,
-    perfil_id: 1,
+    perfil_id: 2,
     telefones: {
       main: '(27)3026-4307',
       mobile: '(27)98133-0708',
@@ -26,7 +26,7 @@ const usuarios = [
     idade: 31,
     salario_real: 9300.92,
     vip: false,
-    perfil_id: 2,
+    perfil_id: 1,
     telefones: {
       main: '(27)3000-3097',
       mobile: '(27)93000-3097'
@@ -40,7 +40,7 @@ const usuarios = [
     idade: 24,
     salario_real: 1330.9,
     vip: false,
-    perfil_id: 2,
+    perfil_id: 1,
     telefones: {
       main: '(27)3026-4000'
     },
@@ -51,11 +51,11 @@ const usuarios = [
 const perfis = [
   {
     id: 1,
-    nome: 'Admininstrador'
+    nome: 'Comum'
   },
   {
     id: 2,
-    nome: 'Comum'
+    nome: 'Admininstrador'
   }
 ]
 
