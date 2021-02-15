470c8e969c73f778e8f39efd0de5e1c336d08e7a
Juliano Costa Silva <apfjuliano@gmail.com>
2021-02-15 02:22:16 -0300

    Validação de email existente

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,12 +1,16 @@
 const { usuarios, proximoId } = require('../data/db')
 
 module.exports = {
-  novoUsuario: (root, { nome, email, idade }) => {
+  // args = { nome, email, idade }
+  novoUsuario: (root, args) => {
+    const emailExistente = usuarios.some((u) => u.email === args.email)
+
+    if (emailExistente) {
+      throw new Error('E-mail já existente')
+    }
     const novo = {
       id: proximoId(),
-      nome,
-      email,
-      idade,
+      ...args,
       perfil_id: 1,
       status: 'ATIVO'
     }
